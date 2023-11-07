9fd6c6806ec4f9c29dd6b70f872ecb45ec5dcb96
Sourav Singha <souravs317>
2023-11-07 14:46:17 +0530

    first commit

---
+++ src/com/sonar/JavaProgram.java
@@ -0,0 +1,36 @@
+package com.sonar;
+
+public class JavaProgram {
+		public static void main(String args[]){
+			String input = "GeeksforGeeks"; 
+			  
+	        // getBytes() method to convert string  
+	        // into bytes[]. 
+	        byte [] strAsByteArray = input.getBytes(); 
+	  
+	        byte [] result =  
+	                   new byte [strAsByteArray.length]; 
+	  
+	        // Store result in reverse order into the 
+	        // result byte[] 
+	        for (int i = 0; i<strAsByteArray.length; i++) 
+	            result[i] =  
+	             strAsByteArray[strAsByteArray.length-i-1]; 
+	  
+	        System.out.println(new String(result)); 
+			
+			int a[]={1,2,3,2,11,15,14,16};
+			System.out.println("Odd Numbers:");
+			for(int i=0;i<a.length;i++){
+				if(a[i]%2!=0){
+					System.out.println(a[i]);
+				}
+			}
+			System.out.println("Even Numbers:");
+			for(int i=0;i<a.length;i++){
+				if(a[i]%2==0){
+					System.out.println(a[i]);
+				}
+			}
+		}
+	}
